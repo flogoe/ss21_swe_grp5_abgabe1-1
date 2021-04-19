a0bfd048abe1b0281a13d85a38c9a2332916f83a
Florian Göbel <florian.goebel@outlook.de>
2021-04-19 14:37:30 +0200

    Dockerimage aktualisiert.

--- scripts/docker-build.ts
+++ scripts/docker-build.ts
@@ -17,7 +17,7 @@
 
 import { exec } from 'shelljs';
 
-const dockerAccount = 'juergenzimmermann';
+const dockerAccount = 'gruppe5';
 //TODO: An Domaene anpassen
 const imageName = 'film';
 const imageTag = '1.0.0';
